eca14b66793fbed055c82bb391f538576050a26c
Mateus-M-Reis <mateusmreis92@gmail.com>
2020-01-10 17:10:52 -0300

    Updates

---
+++ precache-manifest.d74e144cf6d11ea532678a1b7b7ae4d4.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4368bd7a3e1315222d2",
+    "url": "/personal-website/static/css/main.40e98d93.chunk.css"
+  },
+  {
+    "revision": "f4368bd7a3e1315222d2",
+    "url": "/personal-website/static/js/main.f3cace74.chunk.js"
+  },
+  {
+    "revision": "b6d8e3a721968ec40bad",
+    "url": "/personal-website/static/js/runtime~main.93f2c6c9.js"
+  },
+  {
+    "revision": "19261a611f6e6aa1bcab",
+    "url": "/personal-website/static/css/2.35943ae3.chunk.css"
+  },
+  {
+    "revision": "19261a611f6e6aa1bcab",
+    "url": "/personal-website/static/js/2.94f4830d.chunk.js"
+  },
+  {
+    "revision": "19cdb45392e924a4a44a41696767a49b",
+    "url": "/personal-website/static/media/self.19cdb453.jpeg"
+  },
+  {
+    "revision": "fe5730124d13dc6ae4be948ee886e909",
+    "url": "/personal-website/static/media/EE-ufmg-logo.fe573012.svg"
+  },
+  {
+    "revision": "0b63be06db385140920bc504a36b21b6",
+    "url": "/personal-website/static/media/demin-logo.0b63be06.png"
+  },
+  {
+    "revision": "3b3e8f227611b050316727150d4bb3cd",
+    "url": "/personal-website/static/media/demin-logo.3b3e8f22.svg"
+  },
+  {
+    "revision": "67edcff8f089d9f2040bae3eb463d29e",
+    "url": "/personal-website/static/media/demo.67edcff8.svg"
+  },
+  {
+    "revision": "84cf21e61eccd9e047556126bc36d4c7",
+    "url": "/personal-website/static/media/grancv.84cf21e6.png"
+  },
+  {
+    "revision": "48be4a69a3d3bca4417c0c7cdd293c0b",
+    "url": "/personal-website/static/media/retrocalc.48be4a69.png"
+  },
+  {
+    "revision": "da7acb08a3f9d20eda7d85900dde3347",
+    "url": "/personal-website/index.html"
+  }
+];
